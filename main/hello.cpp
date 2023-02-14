4cbc0ec5fc7cdda4579aff9e2c854da18377af8b
ayushi0234 <92812265+ayushi0234@users.noreply.github.com>
2023-02-14 09:55:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World  i am ayushi\n";
+  cout<<"Test\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
